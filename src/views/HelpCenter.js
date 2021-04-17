eba87dfb9278ea29cd552402dfcacbc4dc1e95be
tuul <tultoora@gmail.com>
2021-04-17 08:54:30 +0800

    Baiguullaga

--- src/views/HelpCenter.js
+++ src/views/HelpCenter.js
@@ -1,106 +1,105 @@
 import React, { useEffect, useState } from "react";
 import {
-Button,
-        Card,
-        CardHeader,
-        CardBody,
-        FormGroup,
-        Form,
-        Input,
-        InputGroupAddon,
-        InputGroupText,
-        InputGroup,
-        Modal,
-        CardImg,
-        CardTitle,
-        CardText,
-        CardDeck,
-        CardSubtitle
+    Col,
+    Button,
+    Card,
+    CardHeader,
+    CardBody,
+    FormGroup,
+    Form,
+    Input,
+    InputGroupAddon,
+    InputGroupText,
+    InputGroup,
+    Modal,
+    CardImg,
+    CardTitle,
+    CardText,
+    CardDeck,
+    CardSubtitle,
+    Container,
+    Row,
 } from "reactstrap";
+import helpCorp from "./helpCorp";
 import {
-Container,
-        Row,
-        } from "reactstrap";
-        class HelpCenter extends React.Component {
-        state = {
-        exampleModal: false
-        };
-                toggleModal = state => {
-                this.setState({
-                [state]: !this.state[state]
-                });
-                };
-                render() {
+
+} from "reactstrap";
+
+class HelpCenter extends React.Component {
+    state = {
+        exampleModal: false,
+        corp: helpCorp,
+        cardIndex: 0,
+    };
+    toggleModal = state => {
+        this.setState({
+            [state]: !this.state[state]
+        });
+    };
+    render() {
+        const { corp, cardIndex } = this.state
         return (
-                <>
-<main>
-    <section className="section section-shaped section-lg">
-        <div className="shape shape-style-1 bg-gradient-default">
-            <span />
-            <span />
-            <span />
-            <span />
-            <span />
-            <span />
-            <span />
-            <span />
-        </div>
-        <Container className="pt-lg-7">
-            <Row className="justify-content-center">
-                <h1>Холбогдох байгууллагууд</h1>
-            </Row>
-        </Container>
-    </section>
-    <div>
-        <CardDeck>
-            <Card onClick={() => this.toggleModal("exampleModal")}>
-            <CardImg top width="50px" src="https://bit.ly/3tnQtVP" alt="Card image cap" />
-            <CardBody>
-                <CardTitle tag="h5">Байгууллага</CardTitle>
-            </CardBody>
-            </Card>
-            <Card onClick={() => this.toggleModal("exampleModal")}>
-            <CardImg top width="50px" src="https://bit.ly/3tnQtVP" alt="Card image cap" />
-            <CardBody>
-                <CardTitle tag="h5">Байгууллага</CardTitle>
-            </CardBody>
-            </Card><Card onClick={() => this.toggleModal("exampleModal")}>
-            <CardImg top width="50px" src="https://bit.ly/3tnQtVP" alt="Card image cap" />
-            <CardBody>
-                <CardTitle tag="h5">Байгууллага</CardTitle>
-            </CardBody>
-            </Card>
-        </CardDeck>
-        {/* Modal */}
-        <Modal
-            className="modal-dialog-centered"
-            isOpen={this.state.exampleModal}
-            toggle={() => this.toggleModal("exampleModal")}
-            >
-            <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLabel">
-                    Байгууллага
-                </h5>
-                <button
-                    aria-label="Close"
-                    className="close"
-                    data-dismiss="modal"
-                    type="button"
-                    onClick={() => this.toggleModal("exampleModal")}
-                    >
-                    <span aria-hidden={true}>×</span>
-                </button>
-            </div>
-            <div className="modal-body">
-                <p>Утас: 90909090</p>
-                <p>Фэйсбүүк: Тест</p>
-            </div>
-        </Modal>
-    </div>
-</main>
-</>
-);
-}
+            <>
+                <main>
+                    <section className="section section-shaped section-lg">
+                        <div className="shape shape-style-1 bg-gradient-default">
+                            <span />
+                            <span />
+                            <span />
+                            <span />
+                            <span />
+                            <span />
+                            <span />
+                            <span />
+                        </div>
+                    </section>
+                    <Container>
+                        <Row>
+                            {corp.length && corp.map((report, index) => {
+                                return (
+                                    <Col lg="4">
+                                        <Card style={{ height: '18rem', margin: '1rem' }} onClick={() => this.setState({ exampleModal: true, cardIndex: index })} >
+                                            <CardImg className="help-image"
+                                                xtop width="50px"
+                                                src={report.image}
+                                                alt="corporateimage" />
+                                            <CardBody>
+                                                <CardTitle tag="h5" > {report.name} </CardTitle>
+                                            </CardBody> </Card> </Col>
+                                );
+                            })}
+                        </Row>
+                        <div>
+                            <Modal className="modal-dialog-centered"
+                                isOpen={this.state.exampleModal}
+                                toggle={
+                                    () => this.setState({ exampleModal: !this.state.exampleModal })} >
+                                <div className="modal-header" >
+                                    <h5 className="modal-title"
+                                        id="exampleModalLabel" > {corp[cardIndex].name} </h5> <
+                                            button aria-label="Close"
+                                            className="close"
+                                            data-dismiss="modal"
+                                            type="button"
+                                            onClick={() => this.setState({ exampleModal: false })} >
+                                        <span aria-hidden={true} > × </span>
+                                    </button> </div>
+                                <div className="modal-body">
+                                    <img className="help-image" src={corp[cardIndex].image} />
+                                    <p><b>Үйл ажиллагааны чиглэл: </b> {corp[cardIndex].action}</p>
+                                    <p><b>Фэйсбүүк хаяг: </b> {corp[cardIndex].facebook}</p>
+                                    <p><b>Байгууллагын утас: </b> {corp[cardIndex].phoneNumber}</p>
+                                    <p><b>Зөвлөгөөний утас: </b> {corp[cardIndex].advNumber}</p>
+                                    <p><b>Шуудан хаяг: </b> {corp[cardIndex].email}</p>
+                                    <p><b>Вэбсайт: </b> {corp[cardIndex].website}</p>
+                                </div>
+                            </Modal>
+                        </div>
+                    </Container>
+                </main>
+            </>
+        );
+    }
 }
 
 export default HelpCenter;
--- src/views/Stories.js
+++ src/views/Stories.js
@@ -3,84 +3,84 @@ import { Redirect } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 
 import {
-Card, Button, CardImg, CardTitle, CardText, CardDeck,
-        CardSubtitle, CardBody
+    Card, Button, CardImg, CardTitle, CardText, CardDeck,
+    CardSubtitle, CardBody
 } from 'reactstrap';
 // reactstrap components
 import {
-Container,
-        Row,
+    Container,
+    Row,
 } from "reactstrap";
-        class Stories extends React.Component {
+class Stories extends React.Component {
 
-        render() {
-        const {history}
-        = this.props;
-                const json = '{"title":"Жишээ", "image":"https://bit.ly/3tnQtVP", "text": "sample"}';
-                const obj = JSON.parse(json);
-                return (
-                        <>
-<main>
-    <section className="section section-shaped section-lg">
-        <div className="shape shape-style-1 bg-gradient-default">
-            <span />
-            <span />
-            <span />
-            <span />
-            <span />
-            <span />
-            <span />
-            <span />
-        </div>
-        <Container className="pt-lg-7">
-            <Row className="justify-content-center">
-                <h1>ТҮҮХҮҮД</h1>
-            </Row>
-        </Container>
-    </section>
-    <CardDeck>
-        <Card>
-        <CardImg top width="100%" src={obj.image} alt="Card image cap" />
-        <CardBody>
-            <CardTitle tag="h5">{obj.title}</CardTitle>
-            <CardText>{obj.text}</CardText>
-            <Link to = {{ pathname: '/storiesdetail', state: { title: obj.title, text: obj.text} }}>
-            <Button>
-                Цааш унших
+    render() {
+        const { history }
+            = this.props;
+        const json = '{"title":"Жишээ", "image":"https://bit.ly/3tnQtVP", "text": "sample"}';
+        const obj = JSON.parse(json);
+        return (
+            <>
+                <main>
+                    <section className="section section-shaped section-lg">
+                        <div className="shape shape-style-1 bg-gradient-default">
+                            <span />
+                            <span />
+                            <span />
+                            <span />
+                            <span />
+                            <span />
+                            <span />
+                            <span />
+                        </div>
+                        <Container className="pt-lg-7">
+                            <Row className="justify-content-center">
+                                <h1>ТҮҮХҮҮД</h1>
+                            </Row>
+                        </Container>
+                    </section>
+                    <CardDeck>
+                        <Card>
+                            <CardImg top width="100%" src={obj.image} alt="Card image cap" />
+                            <CardBody>
+                                <CardTitle tag="h5">{obj.title}</CardTitle>
+                                <CardText>{obj.text}</CardText>
+                                <Link to={{ pathname: '/storiesdetail', state: { title: obj.title, text: obj.text } }}>
+                                    <Button>
+                                        Цааш унших
             </Button>
-            </Link>
-        </CardBody>
-        </Card>
-        <Card>
-        <CardImg top width="100%" src={obj.image} alt="Card image cap" />
-        <CardBody>
-            <CardTitle tag="h5">{obj.title}</CardTitle>
-            <CardText>{obj.text}</CardText>
-            <Link to = {{ pathname: '/storiesdetail', state: { title: obj.title, text: obj.text} }}>
-            <Button>
-                Цааш унших
+                                </Link>
+                            </CardBody>
+                        </Card>
+                        <Card>
+                            <CardImg top width="100%" src={obj.image} alt="Card image cap" />
+                            <CardBody>
+                                <CardTitle tag="h5">{obj.title}</CardTitle>
+                                <CardText>{obj.text}</CardText>
+                                <Link to={{ pathname: '/storiesdetail', state: { title: obj.title, text: obj.text } }}>
+                                    <Button>
+                                        Цааш унших
             </Button>
-            </Link>
-        </CardBody>
-        </Card>
-        <Card>
-        <CardImg top width="100%" src={obj.image} alt="Card image cap" />
-        <CardBody>
-            <CardTitle tag="h5">{obj.title}</CardTitle>
-            <CardText>{obj.text}</CardText>
-            <Link to = {{ pathname: '/storiesdetail', state: { title: obj.title, text: obj.text} }}>
-            <Button>
-                Цааш унших
+                                </Link>
+                            </CardBody>
+                        </Card>
+                        <Card>
+                            <CardImg top width="100%" src={obj.image} alt="Card image cap" />
+                            <CardBody>
+                                <CardTitle tag="h5">{obj.title}</CardTitle>
+                                <CardText>{obj.text}</CardText>
+                                <Link to={{ pathname: '/storiesdetail', state: { title: obj.title, text: obj.text } }}>
+                                    <Button>
+                                        Цааш унших
             </Button>
-            </Link>
-        </CardBody>
-        </Card>
-    </CardDeck>
-</main>
-</>
-)
+                                </Link>
+                            </CardBody>
+                        </Card>
+                    </CardDeck>
+                </main>
+            </>
+        )
 
-}
+    }
 }
 
 export default Stories;
